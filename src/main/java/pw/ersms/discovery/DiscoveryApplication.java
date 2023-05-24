55b9dbdfa518b0213d40ca86688bda76405c0965
Truong Giang Do <giangh14cqt@gmail.com>
2023-05-25 00:26:17 +0200

    Add discovery

---
+++ src/main/java/pw/ersms/discovery/DiscoveryApplication.java
@@ -0,0 +1,15 @@
+package pw.ersms.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer // Eureka Server
+public class DiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+
+}
